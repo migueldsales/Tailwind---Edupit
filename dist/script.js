1735bae3839816e9695ae75001ec4aebb4fe2547
AlcariaJoshua <ronjoshua1520@gmail.com>
2023-06-15 15:43:19 +0800

    cheben

--- dist/script.js
+++ dist/script.js
@@ -1,18 +1,37 @@
-const toggleMenu = document.querySelector(".toggle-menu");
-const mobileNav = document.querySelector(".header-nav-drop");
+// const toggleMenu = document.querySelector(".toggle-menu");
+// const mobileNav = document.querySelector(".header-nav-drop");
+// toggleMenu.addEventListener("click", () => {
+//     toggleMenu.classList.toggle("open");
+//     mobileNav.classList.toggle("open");
+// }
+// )
+// const NavDropToggle = document.querySelectorAll(".nav-drop-item h4")
+// const NavDropToggleMenu = document.querySelector(".nav-drop-toggle-menu")
+// const NavDropUlToggle = document.querySelector(".nav-drop-sub-item");
+// NavDropToggle.forEach((drop)=>{
+//     drop.addEventListener("click", () => {
+//         drop.classList.toggle("open");
+//         NavDropToggleMenu.classList.toggle("open")
+//         drop.nextElementSibling.classList.toggle("open");
+//     });
+// });
+const toggleMenu = document.querySelector(".toggle__menu");
+const mobileNav = document.querySelector(".header__nav nav");
 toggleMenu.addEventListener("click", () => {
     toggleMenu.classList.toggle("open");
     mobileNav.classList.toggle("open");
 }
 )
-const NavDropToggle = document.querySelectorAll(".nav-drop-item h4")
-const NavDropToggleMenu = document.querySelector(".nav-drop-toggle-menu")
-const NavDropUlToggle = document.querySelector(".nav-drop-sub-item");
+const NavDropToggle = document.querySelectorAll(".drop__toggle")
+const NavDropToggleMenu = document.querySelector(".drop__content")
+const NavDropUlToggle = document.querySelector(".drop__nav group");
 NavDropToggle.forEach((drop)=>{
     drop.addEventListener("click", () => {
         drop.classList.toggle("open");
-        NavDropToggleMenu.classList.toggle("open")
+        NavDropToggleMenu.classList.toggle("open");
         drop.nextElementSibling.classList.toggle("open");
+        MavDropToggleMenu.classList.remove("open");
+
     });
 });
 
