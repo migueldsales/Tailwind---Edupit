0d153be8e7686fc5275203230e35aefd90ec770a
Miguel Sales <ronjoshua1520@gmail.com>
2023-06-16 14:19:55 +0800

    cheben

--- dist/script.js
+++ dist/script.js
@@ -15,33 +15,69 @@
 //         drop.nextElementSibling.classList.toggle("open");
 //     });
 // });
+
+
+
+// V2
+// const toggleMenu = document.querySelector(".toggle__menu");
+// const mobileNav = document.querySelector(".header__nav nav");
+// toggleMenu.addEventListener("click", () => {
+//     toggleMenu.classList.toggle("open");
+//     mobileNav.classList.toggle("open");
+// }
+// )
+// const NavDropToggle = document.querySelectorAll(".drop__toggle")
+// const NavDropToggleMenu = document.querySelector(".drop__content")
+// const NavDropUlToggle = document.querySelector(".drop__nav group");
+// NavDropToggle.forEach((drop)=>{
+//     drop.addEventListener("click", () => {
+//         drop.classList.toggle("open");
+//         NavDropToggleMenu.classList.toggle("open");
+//         drop.nextElementSibling.classList.toggle("open");
+//         MavDropToggleMenu.classList.remove("open");
+
+//     });
+// });
+
+// const bannerfloat = document.querySelector(".header");
+// window.addEventListener("scroll", () => {
+//   console.log(window.pageYOffset);
+//   if (window.pageYOffset >= 100) {
+//     header.classList.add("header-nav-top");
+//   } else {
+//     header.classList.remove("header-nav-top");
+//   }
+// });
+
+const headerNav = document.querySelectorAll(".drop__nav h4");
+headerNav.forEach((hNav) => {
+  hNav.addEventListener("mousemove", () => {
+    hNav.nextElementSibling.classList.add("drop");
+  });
+  hNav.addEventListener("mouseout", () => {
+    hNav.nextElementSibling.classList.remove("drop");
+  });
+});
+
 const toggleMenu = document.querySelector(".toggle__menu");
-const mobileNav = document.querySelector(".header__nav nav");
+const headerBot = document.querySelector(".header__nav nav");
 toggleMenu.addEventListener("click", () => {
-    toggleMenu.classList.toggle("open");
-    mobileNav.classList.toggle("open");
-}
-)
-const NavDropToggle = document.querySelectorAll(".drop__toggle")
-const NavDropToggleMenu = document.querySelector(".drop__content")
-const NavDropUlToggle = document.querySelector(".drop__nav group");
-NavDropToggle.forEach((drop)=>{
-    drop.addEventListener("click", () => {
-        drop.classList.toggle("open");
-        NavDropToggleMenu.classList.toggle("open");
-        drop.nextElementSibling.classList.toggle("open");
-        MavDropToggleMenu.classList.remove("open");
-
-    });
+  toggleMenu.classList.toggle("open");
+  headerBot.classList.toggle("open");
 });
 
-const bannerfloat = document.querySelector(".header");
-window.addEventListener("scroll", () => {
-  console.log(window.pageYOffset);
-  if (window.pageYOffset >= 100) {
-    header.classList.add("header-nav-top");
-  } else {
-    header.classList.remove("header-nav-top");
-  }
+const dropNav = document.querySelectorAll(".drop__nav h4");
+dropNav.forEach((dropN) => {
+  dropN.addEventListener("click", () => {
+    dropN.nextElementSibling.classList.toggle("open");
+    dropN.querySelector(".drop__toggle").classList.toggle("open");
+  });
 });
 
+const faqs = document.querySelectorAll(".header__item h4");
+faqs.forEach((drop) => {
+  drop.addEventListener("click", () => {
+    drop.nextElementSibling.classList.toggle("open");
+    drop.querySelector("i").classList.toggle("open");
+  });
+});
