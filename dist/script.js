048349b9d138a40b80348648436cf204ccbf3ebf
Miguel Diokno Sales <migueldsales2001@gmail.com>
2023-06-18 20:39:15 +0800

    nine

--- dist/script.js
+++ dist/script.js
@@ -1,55 +1,4 @@
-// const toggleMenu = document.querySelector(".toggle-menu");
-// const mobileNav = document.querySelector(".header-nav-drop");
-// toggleMenu.addEventListener("click", () => {
-//     toggleMenu.classList.toggle("open");
-//     mobileNav.classList.toggle("open");
-// }
-// )
-// const NavDropToggle = document.querySelectorAll(".nav-drop-item h4")
-// const NavDropToggleMenu = document.querySelector(".nav-drop-toggle-menu")
-// const NavDropUlToggle = document.querySelector(".nav-drop-sub-item");
-// NavDropToggle.forEach((drop)=>{
-//     drop.addEventListener("click", () => {
-//         drop.classList.toggle("open");
-//         NavDropToggleMenu.classList.toggle("open")
-//         drop.nextElementSibling.classList.toggle("open");
-//     });
-// });
-
-
-
-// V2
-// const toggleMenu = document.querySelector(".toggle__menu");
-// const mobileNav = document.querySelector(".header__nav nav");
-// toggleMenu.addEventListener("click", () => {
-//     toggleMenu.classList.toggle("open");
-//     mobileNav.classList.toggle("open");
-// }
-// )
-// const NavDropToggle = document.querySelectorAll(".drop__toggle")
-// const NavDropToggleMenu = document.querySelector(".drop__content")
-// const NavDropUlToggle = document.querySelector(".drop__nav group");
-// NavDropToggle.forEach((drop)=>{
-//     drop.addEventListener("click", () => {
-//         drop.classList.toggle("open");
-//         NavDropToggleMenu.classList.toggle("open");
-//         drop.nextElementSibling.classList.toggle("open");
-//         MavDropToggleMenu.classList.remove("open");
-
-//     });
-// });
-
-// const bannerfloat = document.querySelector(".header");
-// window.addEventListener("scroll", () => {
-//   console.log(window.pageYOffset);
-//   if (window.pageYOffset >= 100) {
-//     header.classList.add("header-nav-top");
-//   } else {
-//     header.classList.remove("header-nav-top");
-//   }
-// });
-
-const headerNav = document.querySelectorAll(".drop__nav h4");
+const headerNav = document.querySelectorAll(".drop-nav h4");
 headerNav.forEach((hNav) => {
   hNav.addEventListener("mousemove", () => {
     hNav.nextElementSibling.classList.add("drop");
@@ -59,25 +8,35 @@ headerNav.forEach((hNav) => {
   });
 });
 
-const toggleMenu = document.querySelector(".toggle__menu");
-const headerBot = document.querySelector(".header__nav nav");
+const toggleMenu = document.querySelector(".toggle-menu");
+const headerBot = document.querySelector(".header-wrapper ul");
 toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   headerBot.classList.toggle("open");
 });
 
-const dropNav = document.querySelectorAll(".drop__nav h4");
+const dropNav = document.querySelectorAll(".header-wrapper ul li a");
 dropNav.forEach((dropN) => {
   dropN.addEventListener("click", () => {
     dropN.nextElementSibling.classList.toggle("open");
-    dropN.querySelector(".drop__toggle").classList.toggle("open");
+    dropN.classList.toggle("open");
   });
 });
 
-const faqs = document.querySelectorAll(".header__item h4");
+const faqs = document.querySelectorAll(".header-item h4");
 faqs.forEach((drop) => {
   drop.addEventListener("click", () => {
     drop.nextElementSibling.classList.toggle("open");
     drop.querySelector("i").classList.toggle("open");
   });
+});
+
+const gutter = document.querySelector(".header-wrapper.nav");
+window.addEventListener("scroll", () => {
+  console.log(window.pageYOffset);
+  if (window.pageYOffset >= 300) {
+    gutter.classList.add("top");
+  } else {
+    gutter.classList.remove("top");
+  }
 });
